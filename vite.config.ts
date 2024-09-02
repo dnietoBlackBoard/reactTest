14018a67e6fba3f8e8bbdd9e54faec8558967710
Daniel Nieto <daniel.nieto@anthology.com>
2024-09-02 09:58:39 -0500

    testing vite.config #2

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/", // Set the base URL (for the assets)
+  base: "/", // Set the base URL (for the assets)
   build: {
-    // outDir: "dist",
     assetsDir: "assets",
     emptyOutDir: true,
   },
